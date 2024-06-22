b7febb845755cc5acef51b58ee66174fda9de798
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-06-22 22:23:19 +0900

    [Silver V] Title: 단어 정렬, Time: 768 ms, Memory: 24008 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+        
+        for(int i = 0; i < n; i++)
+            arr[i] = br.readLine();
+        
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override //길이 순서 정렬
+            public int compare(String s1, String s2) {
+                if(s1.length() != s2.length())
+                    return s1.length() - s2.length();
+                //길이가 같다면 알파벳순서로 정렬
+                return s1.compareTo(s2);
+            }
+        });
+        for(int i = 0; i < n; i++){
+            if(i == 0) System.out.println(arr[i]);
+            //중복제거
+            else {
+                if(arr[i].equals(arr[i - 1]))
+                continue;
+                else
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
