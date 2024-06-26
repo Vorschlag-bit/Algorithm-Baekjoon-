91a02c6598720851e9de1a282c71a82f324bf74e
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-06-27 01:37:32 +0900

    [Silver II] Title: 랜선 자르기, Time: 172 ms, Memory: 16752 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -2,14 +2,32 @@ import java.io.*;
 import java.util.*;
 
 class Main{
+    static int[] arr;
+    static int n;
+    static int k;
+    //이진탐색 재귀ver
+    public static long binary_search(long s, long e) {
+        //재귀문에선 최종 end값이 정답이다
+        if(s > e) return e;
+        
+        long mid = (s + e) / 2;
+        int cnt = 0;
+        for(int i = 0; i < k; i++)
+            //작은 경우는 어차피 0이 나오므로 조건절 필요없음
+            cnt += arr[i] / mid;
+        //1. 너무 많이 잘랐음
+        if(cnt < n) return binary_search(s, mid - 1);
+        //2. 너무 적게 잘랐음
+        else return binary_search(mid + 1, e);
+    }  
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
-        int k = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
         
-        int[] arr = new int[k];
+        arr = new int[k];
         long start = 1;
         long end = 0;
         
@@ -17,27 +35,7 @@ class Main{
             arr[i] = Integer.parseInt(br.readLine());
             end = Math.max(arr[i], end);
         }
-        
-        long result = 0;
-        while(start <= end) {
-            int cnt = 0;
-            long mid = (start + end)/2;
-            
-            for(int i = 0; i < k; i++) {
-                //랜선이 이진탐색으로 찾은 값보다 크면 자를 수 있는
-                //몫과 나머지 랜선 길이 모으기
-                if(arr[i] >= mid) {
-                    cnt += arr[i] / mid;
-                }
-            }
-            if(cnt < n) {//자른 횟수가 n보다 적다면 끝을 한 칸 뒤로
-                    end = mid - 1;
-                }
-                else {//크다면 최댓값을 찾고 시작범위를 앞으로 한 칸
-                    result = mid;
-                    start = mid + 1;
-                }
-        }
+        long result = binary_search(start, end);
         System.out.println(result);
     }
 }
