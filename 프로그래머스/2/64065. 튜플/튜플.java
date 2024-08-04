73e85e3368ccb828bd1764888cca438a2388e7b1
이우진 <147306997+Vorschlag-bit@users.noreply.github.com>
2024-08-04 21:42:47 +0900

    [level 2] Title: 튜플, Time: 57.10 ms, Memory: 107 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String s) {
+        s = s.substring(2, s.length() - 2);
+        String[] sets = s.split("},\\{");
+
+        Arrays.sort(sets, (o1, o2) -> o1.length() - o2.length());
+        Set<Integer> ans = new HashSet<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(String str : sets) {
+            for(String num : str.split(",")) {
+                int n = Integer.parseInt(num);
+                if(!ans.contains(n)) {
+                    ans.add(n);
+                    list.add(n);
+                }
+            }
+        }
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
