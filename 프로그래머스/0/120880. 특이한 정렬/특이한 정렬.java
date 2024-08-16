b2342a281e3503d14857e2439c18ac2a44f9396f
이우진 <147306997+Vorschlag-bit@users.noreply.github.com>
2024-08-16 20:47:26 +0900

    [level 0] Title: 특이한 정렬, Time: 7.79 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -1,13 +1,10 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] numlist, int n) {
-        Integer[] arr = Arrays.stream(numlist).boxed().toArray(Integer[]::new);
-        Arrays.sort(arr, (o1, o2) -> {
+        return Arrays.stream(numlist).boxed().sorted(
+        (o1, o2) -> {
             int diff = Math.abs(o1 - n) - Math.abs(o2 - n);
-            if(diff == 0) return o2.compareTo(o1);
-            return diff;
-        });
-    int[] solution = Arrays.stream(arr).mapToInt(Integer::intValue).toArray();
-    return solution;
+            return diff == 0 ? o2.compareTo(o1) : diff;
+        }).mapToInt(Integer::intValue).toArray();
     }
 }
