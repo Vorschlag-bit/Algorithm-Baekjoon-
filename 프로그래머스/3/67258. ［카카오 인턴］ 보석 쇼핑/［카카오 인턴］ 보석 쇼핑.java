d254c22215ce626b4b688468e33a860d26ab06cc
이우진 <147306997+Vorschlag-bit@users.noreply.github.com>
2024-10-06 17:34:46 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 56.93 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -8,32 +8,25 @@ class Solution {
         }
         // 보석의 종류 수
         int start = 0;
-        int end = 0;
         int minStart = 0;
         int minLength = gems.length;
         
-        while(true) {
-            // 모든 종류의 보석을 포함할 때까지
-            if(map.size() < set.size()) {
-                if(end == gems.length) break;
-                map.put(gems[end], map.getOrDefault(gems[end], 0) + 1);
-                end++;
-            }
-            else {
-                // 모든 종류를 다 포함한다면 최소 구간 찾기
+        for(int end = 0; end < gems.length; end++) {
+            map.put(gems[end], map.getOrDefault(gems[end], 0) + 1);
+            
+            while(map.size() == set.size()) {
                 if(end - start < minLength) {
                     minLength = end - start;
                     minStart = start;
                 }
                 
                 map.put(gems[start], map.get(gems[start]) - 1);
-                if(map.get(gems[start]) == 0)
-                    map.remove(gems[start]);
+                if(map.get(gems[start]) == 0) map.remove(gems[start]);
                 start++;
             }
         }
         
-        int[] answer = {minStart + 1, minStart + minLength};
+        int[] answer = {minStart + 1, minStart + minLength + 1};
         return answer;
     }
 }
