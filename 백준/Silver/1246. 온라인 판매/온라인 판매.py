e6c72c858c748e55ec6594d4b420236f814fad3d
이우진 <147306997+Vorschlag-bit@users.noreply.github.com>
2024-12-24 00:11:16 +0900

    [Silver IV] Title: 온라인 판매, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1246. 온라인 판매/온라인 판매.py
+++ 백준/Silver/1246. 온라인 판매/온라인 판매.py
@@ -3,29 +3,19 @@ import sys
 n, m = map(int, sys.stdin.readline().strip().split())
 
 arr = []
-# 최소의 가격은 Pi 중 최솟값부터 시작해서 완전탐색 혹은 이분탐색으로 찾아보기
-# 어느 한 가격(P)이 정해지면(이분이던 완탐이던) 모든 Pi를 순회하면서
-# Pi가 P보다 크거나 같은 경우, 총 달걀 개수에서 -1, Pi가격 판매달걀 개수 + 1
-# 배열을 완전히 다 순회한 후에는 정답과 비교, Pi판매개수 * Pi가 total보다 크다면 Pi와 cnt를 교체
+# 가격을 dictionary로 선언, key, value로 저장하기
+p = {}
+# 각 가격으로 달걀을 팔 때, 그 가격보다 높은 가격을 지불할 의향이 있는 모든 고객에게 판매 가능
+# 정렬된 배열에서 i번째 가격으로 팔 때, (m-i)명의 고객이 구매가능
 for i in range(m):
     arr.append(int(sys.stdin.readline().strip()))
 
 arr.sort()
+# i = 0번째 가격(ex: 2)일 경우ㅡ 달걀 총 개수보다 더 팔 순 없으므로
+# p에 저장되는 값 = (key)2: (value)2 * min(달걀 총 개수, 사람 수)
+for i in range(m):
+    p[arr[i]] = arr[i] * min(n, (m - i))
 
-a = 0
-b = 0
-for i in arr:
-    eggs = n
-    total = 0
-    cnt = 0
-    for price in arr:
-        if eggs > 0:
-            if price >= i:
-                cnt += 1
-                eggs -= 1
-    total = i * cnt
-    if b < total:
-        a = i
-        b = total
-print(f"{a} {b}")
-
+for k, v in p.items():
+    if v == max(p.values()):
+        print(k, v)
