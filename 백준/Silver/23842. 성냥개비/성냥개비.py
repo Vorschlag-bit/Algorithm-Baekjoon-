14e37b23c93b360b240d71fba59a56929f8fb70b
이우진 <147306997+Vorschlag-bit@users.noreply.github.com>
2024-12-30 20:45:29 +0900

    [Silver IV] Title: 성냥개비, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/23842. 성냥개비/성냥개비.py
+++ 백준/Silver/23842. 성냥개비/성냥개비.py
@@ -7,12 +7,9 @@ flag = False
 arr = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6]
 for i in range(10):
     for j in range(10):
-        first = 10 * i + j
-        used = arr[i] + arr[j]
-        if used > n:  # 이미 초과했다면 더 진행할 필요 없음
-            continue
         for k in range(10):
             for l in range(10):
+                first = 10 * i + j
                 second = 10 * k + l
                 num = first + second
 
@@ -20,9 +17,6 @@ for i in range(10):
                     continue
 
                 used = arr[i] + arr[j] + arr[k] + arr[l]
-                if used > n: # 사용한 성냥개비 양 초과
-                    continue
-                
                 result_first = num // 10
                 result_second = num % 10
                 needed = arr[result_first] + arr[result_second]
