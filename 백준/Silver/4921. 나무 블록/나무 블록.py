c7b1b20c3a4198664c2541317939076c4e878fa4
이우진 <147306997+Vorschlag-bit@users.noreply.github.com>
2025-01-08 16:14:45 +0900

    [Silver IV] Title: 나무 블록, Time: 172 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4921. 나무 블록/나무 블록.py
+++ 백준/Silver/4921. 나무 블록/나무 블록.py
@@ -1,56 +1,33 @@
 import sys
+input = sys.stdin.readline
 
-def check(num, idx):
-    match int(num[idx]):
-        case 1:
-            if (int(num[idx + 1]) != 4 and int(num[idx + 1]) != 5) or idx != 0:
-                return False
-            return True
-        case 2:
-            if idx != len(num) - 1:
-                return False
-            return True
-        case 3:
-            if int(num[idx + 1]) != 4 and int(num[idx + 1]) != 5:
-                return False
-            return True
-        case 4:
-            if int(num[idx + 1]) != 2 and int(num[idx + 1]) != 3:
-                return False
-            return True
-        case 5:
-            if int(num[idx + 1]) != 8:
-                return False
-            return True
-        case 6:
-            if int(num[idx + 1]) != 2 and int(num[idx + 1]) != 3:
-                return False
-            return True
-        case 7:
-            if int(num[idx + 1]) != 8:
-                return False
-            return True
-        case 8:
-            if int(num[idx + 1]) != 7 and int(num[idx + 1]) != 6:
-                return False
-            return True
+def check(num):
+    l = len(num)
+    if num[0] != "1" or num[l - 1] != "2":
+        return "NOT"
+    for i in range(l - 1):
+        a = int(num[i])
+        b = int(num[i + 1])
+        if b not in graph[a]:
+            return "NOT"
+    else:
+        return "VALID"
 
+graph = [[] for i in range(9)]
+graph[1] = [4, 5]
+graph[2] = []
+graph[3] = [4, 5]
+graph[4] = [2, 3]
+graph[5] = [8]
+graph[6] = [2, 3]
+graph[7] = [8]
+graph[8] = [7, 6]
 no = 1
 while True:
-    cases = sys.stdin.readline().strip()
-    if int(cases) == 0:
+    num = input().strip()
+    if num == "0":
         break
-    # 처음은 반드시 1 끝은 반드시 2
-    if int(cases[0]) != 1 or int(cases[len(cases) - 1]) != 2:
-        print(f"{no}. NOT")
-        no += 1
-        continue
-    # case 완전 탐색
-    for i in range(len(cases)):
-        if not check(cases, i):
-            print(f"{no}. NOT")
-            no += 1
-            break
-    else:
-        print(f"{no}. VALID")
-        no += 1
+    ans = check(num)
+    print(f"{no}. {ans}")
+    no += 1
+
