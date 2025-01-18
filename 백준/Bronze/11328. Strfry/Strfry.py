b09e9232bc6be3484e7deb1192b0415b65fa5e06
Euxine <bustout@naver.com>
2025-01-18 23:47:19 +0900

    [Bronze II] Title: Strfry, Time: 100 ms, Memory: 34900 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.py
+++ 백준/Bronze/11328. Strfry/Strfry.py
@@ -1,24 +1,9 @@
+from collections import Counter
 import sys
+
 input = sys.stdin.readline
-
 n = int(input().strip())
-
-for i in range(n):
-    dic1 = {}
-    dic2 = {}
+for _ in range(n):
     str1, str2 = input().split()
-    for j in range(len(str1)):
-        abc = str1[j]
-        dic1[abc] = dic1.get(abc, 0) + 1
-    for j in range(len(str2)):
-        abc = str2[j]
-        dic2[abc] = dic2.get(abc, 0) + 1
-    check = True
-    for k in dic1.keys():
-        if k in dic2.keys() and dic1[k] == dic2[k]:
-            continue
-        else:
-            check = False
-            break
-    print("Possible" if check else "Impossible")
-    
+    # Counter 객체로 각 문자의 빈도수를 한 번에 계산
+    print("Possible" if Counter(str1) == Counter(str2) else "Impossible")
