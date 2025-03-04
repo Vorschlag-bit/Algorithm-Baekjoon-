0f50acdf2aadc7f06ce02a568e0145bc50af659f
Euxine <bustout@naver.com>
2025-03-04 15:33:43 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 132 ms, Memory: 36556 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,28 +1,23 @@
 from collections import deque
-import sys
-input = sys.stdin.readline
 
-N, K = map(int, input().split()) #출발 / 도착
-dist = [0] * 100001 #최대 0부터 100000까지의 지점까지 걸리는 횟수 기록
+n,k = map(int, input().split())
 
-#BFS 탐색
-queue = deque()
-queue.append(N) #시작점
+dis = [0] * 100001
+visit = [False] * 100001
 
-# 큐가 빌 때까지 탐색
-while queue:
-    x = queue.popleft()
-    # 도착지점이라면 탐색 종료
-    if x == K:
-        print(dist[x])
+q = deque()
+q.append((0,n))
+visit[n] = True
+
+while q:
+    s,p = q.popleft()
+    if p == k:
+        print(s)
         break
-
-    # x에서 -1 또는 +1 또는 *2 탐색
-    for nextX in (x*2, x-1, x+1):
-        # 0~100000지점 내이고, 방문하지 않았다면
-        if 0 <= nextX <= 100000 and not dist[nextX]:
-            if nextX == x*2:
-                dist[nextX] = dist[x]
+    for i in (p * 2, p - 1, p + 1):
+        if 0 <= i <= 100000 and not visit[i]:
+            visit[i] = True
+            if i == p*2:
+                q.appendleft((s,i))
             else:
-                dist[nextX] = dist[x] + 1 #방문하고 횟수 1늘림
-            queue.append(nextX) #큐에 추가
+                q.append((s+1,i))
