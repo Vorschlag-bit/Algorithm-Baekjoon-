84fc4a09ccd7a533887ac66525307a2b09ef5b6b
Euxine <bustout@naver.com>
2025-03-17 23:03:52 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.07 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/README.md
+++ 프로그래머스/2/81302. 거리두기 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 거리두기 확인하기 - 81302 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302) 
 
 ### 성능 요약
 
-메모리: 9.32 MB, 시간: 0.12 ms
+메모리: 9.15 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 17일 22:39:50
+2025년 03월 17일 23:03:50
 
 ### 문제 설명
 
--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,41 +1,29 @@
+from itertools import combinations
 def solution(places):
-    def check(x,y):
-        return 0<= x < 5 and 0<= y < 5
     answer = []
-    dx = [0,0,1,-1]
-    dy = [1,-1,0,0]
-    hx = [-1,-1,1,1]
-    hy = [1,-1,-1,1]
-    for place in places:
-        flag = True
-        for i in range(5):
-            if not flag:
-                break
-            for j in range(5):
-                if not flag:
-                    break
-                if place[i][j] == 'P':
-                    for d in range(4):
-                        nx, ny = i + dx[d], j +dy[d]
-                        if check(nx,ny) and place[nx][ny] == 'P':
-                            flag = False
-                            break
-                        ax, by = i + 2*dx[d], j + 2*dy[d]
-                        if check(ax,by) and place[ax][by] == 'P' and place[nx][ny] != 'X':
-                            flag = False
-                            break
-                    for d in range(4):
-                        mx,my = i + hx[d], j + hy[d]
-                        if not check(mx,my): continue
-                        if place[mx][my] == 'P':
-                            p1 = place[mx][j]
-                            p2 = place[i][my]
-                            if p1 != 'X' or p2 != 'X':
-                                flag = False
-                                break
-        
-        if flag:
-            answer.append(1)
-        else:
-            answer.append(0)
+    
+    for r in places:
+        p = []
+        for x,i in enumerate(r):
+            for y,j in enumerate(i):
+                if r[x][y] == 'P':
+                    p.append((x,y))
+        check = True
+        for i in combinations(p, 2):
+            x1,y1 = i[0][0], i[0][1]
+            x2,y2 = i[1][0], i [1][1]
+            if abs(x1 - x2) + abs(y1 - y2) <= 2:
+                if x1 == x2:
+                    if r[x1][(y1+y2)//2] != 'X':
+                        check = False
+                        break
+                elif y1 == y2:
+                    if r[(x1+x2)//2][y1] != 'X':
+                        check = False
+                        break
+                else:
+                    if r[x1][y2] != 'X' or r[x2][y1] != 'X':
+                        check = False
+                        break
+        answer.append(1 if check else 0)
     return answer
