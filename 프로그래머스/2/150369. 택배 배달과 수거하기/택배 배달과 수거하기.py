c2acdb1e68377f06c0adb6a69f1770f7e5e32b89
Euxine <bustout@naver.com>
2025-03-22 22:14:30 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 442.91 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,25 +1,11 @@
-def solution(cap, n, ds, ps):
-    ans = 0
-    deli = [[(i+1),ds[i]] for i in range(len(ds)) if ds[i] > 0]
-    pick = [[(i+1),ps[i]] for i in range(len(ps)) if ps[i] > 0]
-    
-    def makeD(stack):
-        dis = 0
-        amount = 0
-        while stack:
-            # 수거 or 배달해야 할 집(idx,개수)
-            target = stack.pop()
-            # 최대 배달 거리
-            if target[0] > dis:
-                dis = target[0]
-            # 개수가 용량보다 크다면 다시 넣기
-            if amount + target[1] > cap:
-                target[1] = (amount + target[1] - cap)
-                stack.append(target)
-                return dis
-            # 아니면 배달(수거)
-            amount += target[1]
-        return dis
-    while deli or pick:
-        ans += max(makeD(deli), makeD(pick))*2
-    return ans
+def solution(cap, n, d, p):
+    answer = 0
+    for i in range(n-2,-1,-1):
+        d[i] += d[i+1]
+        p[i] += p[i+1]
+    t = 0
+    for i in range(n-1,-1,-1):
+        while d[i] > cap*t or p[i] > cap*t:
+            answer += (i+1)*2
+            t += 1
+    return answer
