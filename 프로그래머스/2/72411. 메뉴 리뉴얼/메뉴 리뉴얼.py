acfb9c20fe6fec213348ab0791d5754232ba1a51
Euxine <bustout@naver.com>
2025-04-06 15:15:09 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 1.99 ms, Memory: 9.56 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,27 +1,11 @@
+from collections import Counter
 from itertools import combinations as comb
-from collections import defaultdict
 def solution(orders, course):
-    answer = []
-    c_set = defaultdict(set)
-    # 메뉴 개수에 따른 최대 주문 수를 저장할 dict
-    dic = {c:0 for c in course}
-    for order in orders:
-        for r in course:
-            if r > len(order): continue
-            for combo_menu in comb([char for char in order],r):
-                # ABCFG -> (A,B), (A,C), (A,F), (A,G) ...
-                # 이미 있는 조합 메뉴라면 패스
-                combo_menu = tuple(sorted(combo_menu))
-                cnt = 0
-                for o in orders:
-                    if set(combo_menu).issubset(set(tuple(o))):
-                        cnt += 1
-                if cnt >= 2:
-                    if dic[r] < cnt:
-                        dic[r] = cnt
-                        c_set[r] = {combo_menu}
-                    elif dic[r] == cnt:
-                        c_set[r].add(combo_menu)
+    ans = []
     for r in course:
-        answer += [''.join(menu) for menu in c_set[r]]
-    return sorted(answer)
+        org = []
+        for order in orders:
+            org += [c for c in comb(sorted(order),r)]
+        cnt = Counter(org).most_common()
+        ans += [''.join(k) for k,v in cnt if v > 1 and cnt[0][1] == v]
+    return sorted(ans)
