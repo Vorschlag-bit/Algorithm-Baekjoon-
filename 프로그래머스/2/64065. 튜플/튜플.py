4202c59d64201b1e639be380cfc91e8be9d27702
Euxine <bustout@naver.com>
2025-04-17 19:39:00 +0900

    [level 2] Title: 튜플, Time: 323.24 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -2,12 +2,10 @@ def solution(s):
     s = s[2:-2]
     s = s.split('},{')
     s.sort(key=len)
-    ans = dict()
-    arr = []
+    ans = []
     for unit in s:
-        l = map(int, unit.split(','))
+        l = list(map(int, unit.split(',')))
         for i in l:
-            ans[i] = 0
-    for k in ans.keys():
-        arr.append(k)
-    return arr
+            if i not in ans:
+                ans.append(i)
+    return ans
