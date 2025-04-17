b9c8edb332cf072dcd3fdbd2d7e0d5b152ba5edd
Euxine <bustout@naver.com>
2025-04-18 01:15:46 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 435.23 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,11 +1,12 @@
-def solution(cap, n, d, p):
+def solution(cap, n, deliveries, pickups):
     answer = 0
     for i in range(n-2,-1,-1):
-        d[i] += d[i+1]
-        p[i] += p[i+1]
+        deliveries[i] += deliveries[i+1]
+        pickups[i] += pickups[i+1]
+        
     t = 0
     for i in range(n-1,-1,-1):
-        while d[i] > cap*t or p[i] > cap*t:
+        while t * cap < deliveries[i] or t * cap < pickups[i]:
             answer += (i+1)*2
             t += 1
     return answer
