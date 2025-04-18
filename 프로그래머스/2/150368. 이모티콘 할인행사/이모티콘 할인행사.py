f0feb6ec5fe37ae4d6bd85922717155704db8aa2
Euxine <bustout@naver.com>
2025-04-18 16:46:06 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 1496.44 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -1,37 +1,27 @@
 from itertools import product as perm
-def solution(users, es):
-    # 완전탐색으로 하면, 40 ^ len(es)
-    # 특정 이모티콘이 현재 n% 할인임을 기록하면서 다른 이모티콘의 할인을 적용한 걸 계산해야 함
-    # 최대의 목표를 달성하기 위한 답 => 최대의 할인율 중 최소의 할인율을 찾기
-    # 모든 이모티콘마다 최대의 할인율 중 최소의 할인율을 찾아야 한다.
-    # 특정 이모티콘의 할인율은 다른 이모티콘의 할인율에 영향을 끼침
-    # 데카르트의 곱을 사용해서 모든 수 고려, 이모티콘 1 - n까지 1-40으로 만들 수 있는 경우의 수
+def solution(users, emoticons):
+    # 임티플 최대 가입 수, 최대 매출액
     ans = [0,0]
-    dis = [10,20,30,40]
-    for discount in perm(dis, repeat=len(es)):
-        # [10,10] ~ [40,40]
-        # 원본 가격 배열 복사
-        emos = []
-        # 할인 적용
-        for idx, price in enumerate(es):
-            dis_price = price * (100 - discount[idx]) // 100
-            emos.append(dis_price)
-        # 가입자 수
-        mem = 0
-        # 총 구매금액
+    # 10,20,30,40을 len(emo)만큼 중복순열로 뽑기
+    discount = [10,20,30,40]
+    for dis in perm(discount, repeat=len(emoticons)):
+        # 이모티콘 별로 적용할 할인율 설정
+        plus = 0
         total = 0
-        for user_h,user_m in users:
-            # 총 구매량
+        for user_ratio,user_limit in users:
+            # 유저마다 구매 판별
             amount = 0
-            for idx in range(len(discount)):
-                # 멤버 기준이상이면 구매
-                if discount[idx] >= user_h:
-                    amount += emos[idx]
-            if amount >= user_m:
-                mem += 1
-            else:
-                total += amount
-        # 최대 판별
-        if mem > ans[0] or (mem == ans[0] and total > ans[1]):
-            ans = [mem,total]
+            for idx,price in enumerate(emoticons):
+                ratio = dis[idx]
+                if ratio >= user_ratio:
+                    # 할인 적용한 가격
+                    amount += price - ((price * ratio) // 100)
+                if amount >= user_limit:
+                    plus += 1
+                    amount = 0
+                    break
+            total += amount
+        if ans[0] < plus or (ans[0] == plus and ans[1] < total):
+            ans[0] = plus
+            ans[1] = total
     return ans
