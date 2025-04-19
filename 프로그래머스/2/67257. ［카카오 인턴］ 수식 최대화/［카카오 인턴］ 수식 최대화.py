455b34998d04a2ce4d70da5b996374912f9aa47e
Euxine <bustout@naver.com>
2025-04-19 21:07:37 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.27 ms, Memory: 9.44 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -1,25 +1,28 @@
-from re import split
 from itertools import permutations
 def solution(exp):
     ans = []
-    for priority in permutations(['*','-','+'], 3):
-        nums = list(map(int, split('[\*\-\+]', exp)))
-        operators = [c for c in exp if c in '+-*']
+    for priority in permutations(['*','-','+'],3):
+        nums = exp
         for p in priority:
-            while p in operators:
-                i = operators.index(p)
+            nums = nums.replace(p, ' ')
+        nums = list(map(int, nums.split()))
+        ops = [op for op in exp if op in '*-+']
+        for p in priority:
+            while p in ops:
+                i = ops.index(p)
                 v = 0
+                
                 if p == '*':
-                    v += nums[i] * nums[i+1]
-                elif p == '-':
-                    v += nums[i] - nums[i+1]
+                    v = nums[i] * nums[i+1]
+                elif p == '+':
+                    v = nums[i] + nums[i+1]
                 else:
-                    v += nums[i] + nums[i+1]
+                    v = nums[i] - nums[i+1]
                 nums[i] = v
                 nums.pop(i+1)
-                operators.pop(i)
-        ans.append(nums[0])
-    answer = 0
-    for a in ans:
-        answer = max(answer, abs(a))
-    return answer
+                ops.pop(i)
+        ans.append(abs(nums[0]))
+        a = 0
+        for i in ans:
+            a = max(a,i)
+    return a
