8653292b4653f5a1148daf58da5a18f9144c5637
Euxine <bustout@naver.com>
2025-05-09 13:08:51 +0900

    [level 2] Title: 시소 짝꿍, Time: 12.92 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -2,14 +2,13 @@ from collections import Counter
 def solution(weight):
     ans = 0
     counter = Counter(weight)
-    k = sorted(counter.keys())
-    for w in k:
+    for w in counter:
         if counter[w] > 1:
             ans += counter[w]*(counter[w]-1)//2
     r = [(2,3),(2,4),(3,4)]
-    for w in k:
+    for w in counter:
         for a,b in r:
             t = w*a/b
-            if t in k:
+            if t in counter:
                 ans += counter[t]*counter[w]
     return ans
