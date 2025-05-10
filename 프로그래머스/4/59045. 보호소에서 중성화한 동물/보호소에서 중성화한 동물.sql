17b20dfd32a2cb9c5506a102864230bbe5be71a4
Euxine <bustout@naver.com>
2025-05-10 22:33:29 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,11 @@
+-- 보호소에 들어올 당시에는 중성화되지 않았지만,
+-- 보호소를 나갈 당시에는 중성화된 동물의 아이디와 생물 종, 이름을 조회
+SELECT
+I.ANIMAL_ID,
+I.ANIMAL_TYPE,
+I.NAME
+FROM ANIMAL_INS I
+LEFT JOIN ANIMAL_OUTS O
+ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE I.SEX_UPON_INTAKE != O.SEX_UPON_OUTCOME
+ORDER BY 1
