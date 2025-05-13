2f3b5f1d8d457251532175fee734a9d425247711
Euxine <bustout@naver.com>
2025-05-13 13:32:49 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 473.47 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
@@ -5,7 +5,7 @@ def solution(num):
     for i in range(l-1,-1,-1):
         while stack and stack[-1] <= num[i]:
             stack.pop()
-        if not stack: ans[i] = -1
+        if not stack: pass
         else: ans[i] = stack[-1]
         stack.append(num[i])
     return ans
