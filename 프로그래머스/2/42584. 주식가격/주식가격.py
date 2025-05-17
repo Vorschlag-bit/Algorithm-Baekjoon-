a7acc7fc9d308940c28aa6a56edf48d37b54f09b
Euxine <bustout@naver.com>
2025-05-18 00:03:40 +0900

    [level 2] Title: 주식가격, Time: 26.21 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,14 +1,13 @@
 def solution(prices):
     stack = []
-    arr = []
+    ans = [0]*len(prices)
     # pop되는 순간이 떨어지는 시간
     for i,p in enumerate(prices):
         while stack and stack[-1][1] > p:
             idx,pr = stack.pop()
-            arr.append((idx,i-idx))
+            ans[idx] = i-idx
         stack.append((i,p))
     while stack:
         idx,pr = stack.pop()
-        arr.append((idx,len(prices)-idx-1))
-    arr.sort(key=lambda x: x[0])
-    return [arr[i][1] for i in range(len(prices))]
+        ans[idx] = len(prices)-idx-1
+    return ans
