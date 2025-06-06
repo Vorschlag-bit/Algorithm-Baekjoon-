56dc2e2130dc9e7c0032503b77a1153aacfc5bca
Euxine <bustout@naver.com>
2025-06-06 15:17:02 +0900

    [level 3] Title: 섬 연결하기, Time: 0.12 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -2,22 +2,20 @@ import heapq
 from collections import defaultdict
 def solution(n, costs):
     q = []
-    cost = [float('inf')]*n
+    # 그니까;; 0에서 출발했을 때 최솟값이라 생각했는데....
     graph = defaultdict(list)
     for a,b,c in costs:
         graph[a].append((b,c))
         graph[b].append((a,c))
     heapq.heappush(q,(0,0))
-    visit = [False]*n
-    cost[0] = 0
     ans = 0
+    visit = [False]*n
     while q:
         co,cur = heapq.heappop(q)
         if visit[cur]: continue
         visit[cur] = True
         ans += co
         for nxt,c in graph[cur]:
-            if not visit[nxt] and cost[nxt] > c:
-                cost[nxt] = c
+            if not visit[nxt]:
                 heapq.heappush(q,(c,nxt))
     return ans
