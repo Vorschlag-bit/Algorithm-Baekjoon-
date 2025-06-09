9056ed212eea9a070a983a69adfa70108bc7bccc
Euxine <bustout@naver.com>
2025-06-09 14:08:27 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,14 +1,7 @@
-from collections import Counter
-def solution(lottos, win):
-    lc = Counter(lottos)
-    # 0을 제외하고 맞춘 번호의 개수(n)  7 - n -> rank
-    wc = Counter(win)
+def solution(lottos, win_nums):
+    l = [6,6,5,4,3,2,1]
+    c_0 = lottos.count(0)
     cnt = 0
-    for l in lc.keys():
-        if l in wc.keys():
-            cnt += 1
-    worst = 7 - cnt if cnt != 0 else 6
-    # cnt = 0인데, lc[0] = 0
-    best = 7 - cnt - lc.get(0,0)
-    if best == 7: best = 6
-    return [best,worst]
+    for n in lottos:
+        if n in win_nums: cnt+=1
+    return l[c_0+cnt], l[cnt]
