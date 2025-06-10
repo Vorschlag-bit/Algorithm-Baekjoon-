926c33a23655b0a7f4c14a1be73afa7af7c2dca2
Euxine <bustout@naver.com>
2025-06-10 15:16:51 +0900

    [level 3] Title: 불량 사용자, Time: 206.36 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -1,24 +1,22 @@
-def check(u,b):
-    if len(u) != len(b):
-        return False
-    for i,char in enumerate(b):
-        if char != '*' and char != u[i]: return False
-    return True
-
-def dfs(idx,user,ban,path,visit,result):
-    if idx == len(ban):
-        result.add(frozenset(path))
-        return
-    for i,usr in enumerate(user):
-        if not visit[i] and check(usr,ban[idx]):
-            visit[i] = True
-            path.append(usr)
-            dfs(idx+1,user,ban,path,visit,result)
-            visit[i] = False
-            path.pop()
-
-def solution(user, ban):
-    visit = [False]*len(user)
-    result = set()
-    dfs(0,user,ban,[],visit,result)
-    return len(result)
+from itertools import permutations as perm
+def solution(user_id, banned_id):
+    ans = set()
+    n = len(user_id)
+    m = len(banned_id)
+    def check(p_id,b_id):
+        flag = True
+        for p,b in zip(p_id,b_id):
+            if len(p) != len(b):
+                flag = False
+                break
+            for pchar,bchar in zip(p,b):
+                if bchar != '*' and bchar != pchar:
+                    flag = False
+                    break
+        return flag
+    # 순열로 풀기
+    for p in perm(user_id, m):
+        if check(p,banned_id):
+            key = frozenset(p)
+            ans.add(key)
+    return len(ans)
