fac0430d50b4563d2b2b81cb25d2d852e71f1ccd
Euxine <bustout@naver.com>
2025-06-10 19:14:08 +0900

    [level 3] Title: 주사위 고르기, Time: 3822.02 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -1,47 +1,45 @@
-from itertools import combinations as comb, product as prod
-from bisect import bisect_left
-def getWin(mydice,yourdice):
-    # yd = [[3,3,3,3,3,3],[1,2,3,4,5,6]]
-    # 이 주사위로 만들 수 있는 합의 1차원 배열 생성
-    win = 0
-    l = list(range(6))
-    b = []
-    for i in prod(l, repeat=len(yourdice)):
-        # i = (0,0) - (5,5)
-        value = 0
-        for j in range(len(yourdice)):
-            value += yourdice[j][i[j]]
-        b.append(value)
-    b.sort()
-    for i in prod(l, repeat=len(mydice)):
-        # (0,0) - (5,5)
-        # (0,0)의 합을 구하고, 그게 상대방의 배열에서 이길 수 있는 수 구하기
-        value = 0
-        for j in range(len(mydice)):
-            value += mydice[j][i[j]]
-        win += bisect_left(b,value)
-    return win
-
+from itertools import combinations as comb
+from itertools import product as prod
+from bisect import bisect_left as binary
+def duel(mydice,yourdice,dice):
+    cnt = 0
+    # 각 주사위로 승부([3,3,3,4,4,4,4] vs [1,2,3,4,5,6])
+    # 주사위로 굴려서 만들 수 있는 모든 수 저장
+    your_scores = []
+    idx = [i for i in range(6)]
+    for p in prod(idx, repeat=len(yourdice)):
+        # (0..0) - (5..5)
+        # yd의 #1, #2
+        score = 0
+        for i in range(len(yourdice)):
+            score += yourdice[i][p[i]]
+        your_scores.append(score)
+    # 오름차순 정렬
+    your_scores.sort()
+    # 내 주사위로 만들 수 있는 모든 점수
+    for p in prod(idx, repeat=len(mydice)):
+        score = 0
+        for i in range(len(mydice)):
+            score += mydice[i][p[i]]
+        # 내 점수가 네 주사위에서 이길 수 있는 경우의 수
+        cnt += binary(your_scores,score)
+    return cnt
 def solution(dice):
-    # 서로 주사위를 반반 나누고, 나올 수 있는 합의 배열을 만든 후, 상대방 배열에서
-    # 이진탐색으로 이길 수 있는 인덱 = 승 개수
-    maxWin = 0
-    answer = []
-    size = list(range(len(dice)))
-    for md in comb(size, len(dice)//2):
-        yd = []
-        for n in size:
-        # 내게 없는 주사위 idx는 상대 꺼
-            if n not in md:
-                yd.append(n)
-        m = [dice[i] for i in md]
-        y = [dice[i] for i in yd]
-        
-        wins = getWin(m,y)
-        
-        if wins > maxWin:
-            maxWin = wins
-            answer = md
-        
-    answer = [x+1 for x in answer]
-    return answer
+    ans = []
+    # 최대 승수
+    max_win = 0
+    n = len(dice)
+    arr = [i for i in range(n)]
+    for mine in comb(arr, n//2):
+        # 상대 주사위
+        your = []
+        for a in arr:
+            if a not in mine: your.append(a)
+        mydice = [dice[i] for i in mine]
+        yourdice = [dice[i] for i in your]
+        wins = duel(mydice, yourdice,dice)
+        if (wins > max_win):
+            max_win = wins
+            ans = mine
+    ans = [a+1 for a in ans]
+    return ans
