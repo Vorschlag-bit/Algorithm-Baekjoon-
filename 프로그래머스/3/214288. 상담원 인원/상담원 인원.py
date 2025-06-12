f97e615d3f1184c54150c695c78cd89bce0a8932
Euxine <bustout@naver.com>
2025-06-12 15:59:40 +0900

    [level 3] Title: 상담원 인원, Time: 461.52 ms, Memory: 9.65 MB -BaekjoonHub

--- 프로그래머스/3/214288. 상담원 인원/상담원 인원.py
+++ 프로그래머스/3/214288. 상담원 인원/상담원 인원.py
@@ -2,33 +2,42 @@ from collections import defaultdict
 import heapq
 def split(n,k):
     result = []
-    def dfs(path,idx):
-        if idx == k:
-            if sum(path) == n:
+    def dfs(path, rest_s, rest_k):
+        # path = 지금까지 고른 배열
+        # rest_s = 남은 합
+        # rest_k = 남은 k
+        if rest_k == 0:
+            if rest_s == 0:
                 result.append(path[:])
             return
-        for i in range(1,n+1):
-            if sum(path) + i + (k - idx - 1) <= n:
-                dfs(path+[i], idx+1)
-    dfs([],0)
+        min_v = 1
+        max_v = rest_s - (rest_k-1)
+        if max_v < min_v: return
+        for value in range(min_v, max_v+1):
+            path.append(value)
+            dfs(path, rest_s - value, rest_k-1)
+            path.pop()
+    dfs([],n,k)
     return result
 def solution(k, n, reqs):
     ans = float('inf')
-    # 멘토 수n를 상담 유형k만큼 나누기
-    # 그렇게 나눈 걸 바탕으로 heap을 사용해서 기다린 시간 계산
-    for com in split(n,k):
-        com = [0] + com
-        dic = defaultdict(list)
-        wait = 0
+    # 기다린 시간의 합이 최소
+    # n명의 멘토를 k로 최소 몫 1이상 나누기
+    # (시작, 끝)으로 넣고
+    # if len(stack) == s[c] and stack[-1]: 기다리는 시간 계산해서 더해주기
+    for s in split(n,k):
+        w = 0
+        s = [0] + s
+        counsling = defaultdict(list)
         for i in range(1,k+1):
-            for _ in range(com[i]):
-                heapq.heappush(dic[i], 0)
+            # 각 상담 유형마다 개수에 맞는 상담사 heap을 생성(끝나는 시간 기준)
+            for _ in range(s[i]):
+                heapq.heappush(counsling[i],0)
         for a,b,c in reqs:
-            # c번 유형 상담을 a분 ~ b분 동안 상담을 요청
-            end = heapq.heappop(dic[c])
+            end = heapq.heappop(counsling[c])
             if end > a:
-                wait += end - a
+                w += end - a
                 a = end
-            heapq.heappush(dic[c],a+b)
-        ans = min(ans, wait)
+            heapq.heappush(counsling[c],a+b)
+        ans = min(ans,w)
     return ans
