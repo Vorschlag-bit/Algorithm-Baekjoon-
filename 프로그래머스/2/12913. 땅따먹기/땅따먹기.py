f64894b33c9fad5585474e1a06fe611ad4b16bd9
Lee Woo Jin <bustout@naver.com>
2025-06-16 15:35:20 +0900

    [level 2] Title: 땅따먹기, Time: 405.00 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,6 +1,6 @@
 def solution(land):
-    n = len(land)
-    for i in range(1,n):
+    # 누적합
+    for i in range(1,len(land)):
         for j in range(4):
-            land[i][j] += max(land[i-1][k] for k in range(4) if k!= j)
+            land[i][j] += max(land[i-1][k] for k in range(4) if k != j)
     return max(land[-1])
