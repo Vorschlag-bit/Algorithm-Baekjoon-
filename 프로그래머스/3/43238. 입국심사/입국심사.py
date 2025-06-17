66769d95e6446c63427568901a9b11c6b52c0c82
Lee Woo Jin <bustout@naver.com>
2025-06-17 18:29:36 +0900

    [level 3] Title: 입국심사, Time: 1068.72 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,18 +1,14 @@
 def solution(n, times):
-    ans = float('inf')
-    # 이진탐색의 대상이 되는 건? => 모든 사람이 심사 소요 시간
-    # 모든 사람의 심사 소요 최대 시간? => 1 ~ 
+    # 시간에 대한 이진탐색
+    # 특정 시간 t에 대해서 모든 입국을 수행할 수 있는지 체크
     times.sort()
-    r = n * times[-1]
-    l = 1
+    l,r = 0, times[-1]*n
     while l <= r:
-        m = (l+r)//2
+        m = (l+r) // 2
+        # m 시간 안에 모든 입국을 처리할 수 있는가?
         cnt = 0
-        for i in range(len(times)-1,-1,-1):
-            cnt += m // times[i]
-        if cnt >= n:
-            r = m - 1
-            ans = min(ans,m)
-        else:
-            l = m + 1
-    return ans
+        for t in times:
+            cnt += m // t
+        if cnt >= n: r = m - 1
+        else: l = m + 1
+    return l
