115a73e8a59b647145a229b10aca0ca19ea24c2b
Lee Woo Jin <bustout@naver.com>
2025-06-24 15:07:47 +0900

    [level 3] Title: 최고의 집합, Time: 0.67 ms, Memory: 9.7 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
@@ -1,11 +1,10 @@
 def solution(n, s):
     ans = []
-    if n > s: return [-1]
-    q = s//n
-    r = s%n
-    for _ in range(r):
-        ans.append(q+1)
-    for _ in range(n-r):
-        ans.append(q)
-    ans.sort()
-    return ans
+    # n개의 원소 합이 s면서 곱 최대 => ele 간의 격차 최소화
+    # s를 n으로 나누고 부족한 건 n-1에 나눠주기
+    if s < n: return [-1]
+    q,r = divmod(s,n)
+    arr = [q]*n
+    for i in range(r):
+        arr[i] += 1
+    return sorted(arr) if arr else [-1]
