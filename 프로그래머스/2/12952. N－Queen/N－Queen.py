367e7f545b1217e806e1c1a0d3c4c7065e2d89fb
Lee Woo Jin <bustout@naver.com>
2025-07-03 11:20:13 +0900

    [level 2] Title: N-Queen, Time: 2021.89 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -1,6 +1,6 @@
 def solution(n):
-    # 조합을 저장할 set
-    visit = set()
+    # 개수
+    ans = 0
     def check(i,j,col,d1,d2):
         # 행, 열, 대각선 집합에 n이 속하는지
         if j in col: return False
@@ -8,12 +8,12 @@ def solution(n):
         if (i+j) in d2: return False
         return True
     
-    # 행, 1-n**2 list, 금지열, 금지대각선 2가지
-    def dfs(i,path,col,d1,d2):
+    # 행,queen 개수,금지열,금지대각선 2가지
+    def dfs(i,cnt,col,d1,d2):
+        nonlocal ans
         # 만약 idx == n and len(path) == n이면 visit에 추가
-        if i == n and len(path) == n:
-            key = frozenset(path)
-            visit.add(key)
+        if i == n and cnt == n:
+            ans += 1
             return
         # idx행에서 j열에 q을 둘 수 있는지 판단
         for j in range(n):
@@ -21,11 +21,11 @@ def solution(n):
                 col.add(j)
                 d1.add((i-j))
                 d2.add((i+j))
-                dfs(i+1,path+[(i,j)],col,d1,d2)
+                dfs(i+1,cnt+1,col,d1,d2)
                 col.remove(j)
                 d1.remove((i-j))
                 d2.remove((i+j))
         # idx행에 둘 수 있는 곳이 없다면 return
         return
-    dfs(0,[],set(),set(),set())
-    return len(visit)
+    dfs(0,0,set(),set(),set())
+    return ans
