bc316a5c70fe99fc22596cab528e49fcbdab0a16
Lee Woo Jin <bustout@naver.com>
2025-08-04 11:43:09 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 2670.07 ms, Memory: 9.57 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
@@ -1,66 +1,55 @@
 from collections import defaultdict
-
 def solution(points, routes):
     ans = 0
-    directions = [(1,0), (-1,0), (0,1), (0,-1)]
-    
-    # 목표 번호 = key, 좌표 = value
+    directions = [(1,0),(-1,0),(0,1),(0,-1)]
+    # 물류센터 번호 - k, 좌표 - v
     p = dict()
-    for i, (r, c) in enumerate(points):
-        p[i + 1] = (r, c)
-    
-    # robot 현재 위치
-    robot = [list(p[r[0]]) for r in routes]
-    
-    # robot 목표 리스트
-    robot_dest = []
+    # robot 번호 - k, 다음 목표 좌표 - v
+    robot_dest = defaultdict(list)
     rs = set()
-    for i, r in enumerate(routes):
-        dest_list = []
-        for j in range(1, len(r)):
-            dest_list.append(p[r[j]])
-        robot_dest.append(dest_list)
-        if len(dest_list) > 0:  # 목적지가 있는 로봇만 활성화
-            rs.add(i)
-    
-    # 초기 위치에서의 충돌 체크
+    for i,point in enumerate(points):
+        p[i+1] = tuple(point)
+    # robot 번호 - k, 좌표 - v
+    robot = [list(p[r[0]]) for r in routes]
+    for idx,r in enumerate(routes):
+        each_list = []
+        for i in range(1,len(r)):
+            each_list.append(p[r[i]])
+        robot_dest[idx] = each_list
+        rs.add(idx)
+    # 시작부터 충돌확인
     position = defaultdict(int)
-    for i, pos in enumerate(robot):
-        k = tuple(pos)
+    for i,r in enumerate(robot):
+        k = tuple(r)
         position[k] += 1
-    
     for v in position.values():
-        if v > 1:
-            ans += 1
-    
-    # 시뮬레이션
+        if v > 1: ans += 1
+    print(robot_dest)
     while len(rs) > 0:
-        robot_list = list(rs)
+        moved_robot = []
+        robots = list(rs)
         rs.clear()
-        moved_robots = []  # 이번 턴에 움직인 로봇들
-        
-        # 각 로봇 이동
-        for i in robot_list:
+        for i in robots:
+            # i = 로봇 번호
             target = robot_dest[i][0]
             cur = robot[i]
             for d in directions:
-                nx, ny = cur[0] + d[0], cur[1] + d[1]
-                if 0 < nx and 0 < ny and abs(target[0] - nx) + abs(target[1] - ny) < abs(target[0] - cur[0]) + abs(target[1] - cur[1]):
-                    robot[i] = [nx, ny]
-                    moved_robots.append(i)  # 움직인 로봇 기록
+                nx,ny = cur[0] + d[0], cur[1] + d[1]
+                if 0 < nx and 0 < ny and abs(nx-target[0])+abs(ny-target[1]) < abs(cur[0]-target[0])+abs(cur[1]-target[1]):
+                    robot[i] = [nx,ny]
+                    moved_robot.append(i)
                     break
-            if tuple(robot[i]) == target:
+            # 도착을 했다면 제거
+            if target == tuple(robot[i]):
                 robot_dest[i].pop(0)
-            if len(robot_dest[i]) > 0: 
+            # robot_dest[i]가 남았다면 추가
+            if len(robot_dest[i]) > 0:
                 rs.add(i)
-        
-        # 이번 턴에 움직인 모든 로봇들로 충돌 체크
-        pos = {}
-        for r in moved_robots:
+        # 움직인 로봇들만 충돌 계산
+        position = defaultdict(int)
+        for r in moved_robot:
             k = tuple(robot[r])
-            pos[k] = pos.get(k, 0) + 1
-        for v in pos.values():
-            if v > 1:
-                ans += 1
-
+            position[k] += 1
+        for v in position.values():
+            if v > 1: ans += 1
     return ans
