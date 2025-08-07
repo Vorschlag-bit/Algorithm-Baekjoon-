4e8b696aecbaef5bc488cbae0aafc459d17a6949
Lee Woo Jin <bustout@naver.com>
2025-08-07 15:36:00 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 496.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -1,49 +1,40 @@
-from collections import deque
+from collections import deque,defaultdict
 def solution(land):
+    ans = 0
+    directions = [(0,1),(1,0),(0,-1),(-1,0)]
     n = len(land)
     m = len(land[0])
-    id_map = [[-1] * m for _ in range(n)]
-    ans = 0
-    dx,dy = [0,0,1,-1], [1,-1,0,0]
-    # n번째 석유가 갖고 있는 총 시추량
-    oils = dict()
-    node = 0
-    def check(x,y):
-        return 0 <= x < n and 0 <= y < m
-    
-    def bfs(x,y,no):
-        q = deque([(x,y)])
-        visit[x][y] = True
-        cnt = 1
-        id_map[x][y] = no
+    size = defaultdict(int)
+    cnt = 1
+    def bfs(x,y,cnt):
+        s = 1
+        q = deque()
+        land[x][y] += cnt
+        q.append((x,y))
         while q:
             cx,cy = q.popleft()
-            for i in range(4):
-                nx,ny = cx + dx[i], cy + dy[i]
-                if not check(nx,ny): continue
-                if not visit[nx][ny] and land[nx][ny] == 1:
-                    visit[nx][ny] = True
-                    id_map[nx][ny] = no
-                    cnt += 1
+            for d in directions:
+                nx,ny = cx+d[0],cy+d[1]
+                if 0 <= nx < n and 0 <= ny < m and land[nx][ny] == 1:
+                    land[nx][ny] += cnt
+                    s += 1
                     q.append((nx,ny))
-        oils[no] = cnt
-        return
-    visit = [[False] * m for _ in range(n)]
-    # 석유 매장 맵, 양 dict 초기화
+        size[land[x][y]] = s
+        
+    for i in range(n):
+        for j in range(m):
+            if land[i][j] == 1:
+                bfs(i,j,cnt)
+                cnt += 1
+    # land[i][j] != 0 -> 시추 번호
     for j in range(m):
+        visit = set()
+        amount = 0
+        # 열을 순회하면서 판단하기
         for i in range(n):
-            if land[i][j] == 1 and not visit[i][j]:
-                bfs(i,j,node)
-                node += 1
-    
-    for j in range(m):
-        oil = 0
-        visited = set()
-        for i in range(n):
-            oil_no = id_map[i][j]
-            if oil_no != -1 and oil_no not in visited:
-                visited.add(oil_no)
-                oil += oils[oil_no]
-        ans = max(ans,oil)
-    
+            # 이미 시추한 적이 있다면 pass
+            if land[i][j] != 0 and land[i][j] not in visit:
+                amount += size[land[i][j]]
+                visit.add(land[i][j])
+        ans = max(ans,amount)
     return ans
