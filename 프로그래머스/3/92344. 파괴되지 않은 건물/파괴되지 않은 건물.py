5c47b18f8cde66f686605ed913f7e6d1301e543c
Lee Woo Jin <bustout@naver.com>
2025-10-06 19:21:00 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 823.30 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -1,31 +1,24 @@
 def solution(board, skill):
+    ans = 0
     n = len(board)
     m = len(board[0])
-    answer = 0
-    # 누적합 배열은 행,열 모두 크기가 +1이여야 한다.
-    diff = [[0] * (m + 1) for _ in range(n + 1)]
-    
-    for typ,r1,c1,r2,c2,dg in skill:
-        if typ == 1: dg = -dg
-        
-        diff[r1][c1] += dg
-        diff[r2+1][c1] -= dg
-        diff[r1][c2+1] -= dg
-        diff[r2+1][c2+1] += dg
-    
-    # 행부터 누적합 시작(c1 + 1 ~ c2 + 1)
-    for r in range(n):
-        for c in range(1, m + 1):
-            diff[r][c] += diff[r][c-1]
-        
-    # 열 누적합 시작(r1 + 1 ~ r2 + 1)
-    for c in range(m):
-        for r in range(1, n + 1):
-            diff[r][c] += diff[r-1][c]
-    
+    diff = [[0] * (m+1) for _ in range(n+1)]
+    # 행 -> 열 순으로 반영
+    for t,r1,c1,r2,c2,d in skill:
+        # 공격
+        if t == 1: d = -d
+        diff[r1][c1] += d
+        diff[r2+1][c2+1] += d
+        diff[r1][c2+1] -= d
+        diff[r2+1][c1] -= d
     for i in range(n):
         for j in range(m):
-            if board[i][j] + diff[i][j] > 0:
-                answer += 1
-    
-    return answer
+            diff[i][j+1] += diff[i][j]
+    for j in range(m):
+        for i in range(n):
+            diff[i+1][j] += diff[i][j]
+    for i in range(n):
+        for j in range(m):
+            board[i][j] += diff[i][j]
+            if board[i][j] > 0: ans += 1
+    return ans
