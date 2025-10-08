be259fdc7338347df5dfc3ae2b0610bac7d3551e
Lee Woo Jin <bustout@naver.com>
2025-10-09 00:51:15 +0900

    [level 3] Title: 합승 택시 요금, Time: 85.79 ms, Memory: 14.6 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,44 +1,55 @@
-from collections import defaultdict
 import heapq
-def dijkstra(s,dest,visit,graph):
+from collections import defaultdict
+def get_d(dis,s,graph):
+    dis[s] = 0
     q = []
-    visit[s] = 0
-    heapq.heappush(q,(0,s))
+    heapq.heappush(q,(s,0))
     while q:
-        cost, cur = heapq.heappop(q)
-        if cur == dest:
-            return cost
-        if visit[cur] < cost:
-            continue
-        for nxt,c in graph[cur]:
-            if visit[nxt] < cost + c:
-                continue
-            visit[nxt] = cost + c
-            heapq.heappush(q,(cost + c,nxt))
-    return 0
-    
+        cur,cd = heapq.heappop(q)
+        if dis[cur] < cd: continue
+        for nxt,cost in graph[cur]:
+            if dis[nxt] <= cost + cd: continue
+            dis[nxt] = cost + cd
+            heapq.heappush(q,(nxt,cost+cd))
+
+def get_cost(t,s,graph,n):
+    dis = [float('inf')] * (n+1)
+    dis[s] = 0
+    q = []
+    heapq.heappush(q,(s,0))
+    while q:
+        cur,cd = heapq.heappop(q)
+        if dis[cur] < cd: continue
+        for nxt,cost in graph[cur]:
+            if dis[nxt] <= cd + cost: continue
+            dis[nxt] = cost + cd
+            heapq.heappush(q,(nxt,cost+cd))        
+    return dis[t]
+            
 def solution(n, s, a, b, fares):
-    ans = float('inf')
-    # fares = [c,d,f] c->d f비용
-    # a만의 최소 비용 노드 [] + 비용
-    # b만의 최소 비용 노드 [] + 비용
     graph = defaultdict(list)
     for c,d,f in fares:
         graph[c].append((d,f))
         graph[d].append((c,f))
-    visit = [float('inf')]*(n+1)
-    # a,b 합승하지 않을 떄의 가격
-    acost = dijkstra(s,a,visit,graph)
-    bcost = dijkstra(s,b,visit,graph)
-    ans = min(ans, acost+bcost)
-    # 둘이 갈라지는 지점 정하고 지점에서부터 a,b 각각 가는 걸 생각
-    for k in range(1,n+1):
-        if k == s: continue
-        # 갈라지는 지점까지의 비용
-        cost = visit[k]
-        avisit = [float('inf')] * (n+1)
-        bvisit = [float('inf')] * (n+1)
-        cost += dijkstra(k,a,avisit,graph)
-        cost += dijkstra(k,b,bvisit,graph)
-        ans = min(ans, cost)
+    # 1. 출발지에서 합승 마지막 지점까지 dijk
+    d = [float('inf')] * (n+1)
+    get_d(d,s,graph)
+    # 각각 최소 가격으로 집 갈 경우
+    ans = get_cost(a,s,graph,n) + get_cost(b,s,graph,n)
+    # 2. A에서 모든 곳까지
+    ad = [float('inf')] * (n+1)
+    get_d(ad,a,graph)
+    # 3. B에서 모든 곳까지
+    bd = [float('inf')] * (n+1)
+    get_d(bd,b,graph)
+    for i in range(1,n+1):
+        if s == i: continue
+        # 중간 지점 찾기
+        base = d[i]
+        # 중간 지점 못 찾으면 pass
+        if base == float('inf'): continue
+        ta = ad[i]
+        tb = bd[i]
+        if ta != float('inf') and tb != float('inf'):
+            ans = min(ans,tb+ta+base)
     return ans
