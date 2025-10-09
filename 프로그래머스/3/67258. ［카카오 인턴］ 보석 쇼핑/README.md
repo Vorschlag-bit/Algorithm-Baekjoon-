cf52d18ec40b5e22341a5fd9e22f622e55b53d25
Lee Woo Jin <bustout@naver.com>
2025-10-09 17:04:46 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 62.77 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/README.md
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/README.md
@@ -1,10 +1,10 @@
 # [level 3] [카카오 인턴] 보석 쇼핑 - 67258 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67258?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67258) 
 
 ### 성능 요약
 
-메모리: 16.9 MB, 시간: 59.22 ms
+메모리: 16.8 MB, 시간: 62.77 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 17일 20:50:35
+2025년 10월 09일 17:04:43
 
 ### 문제 설명
 
--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,20 +1,25 @@
 def solution(gems):
-    gem = set()
-    l = 0
-    minStart = 0
-    minL = len(gems)
+    ans = []
+    # 진열된 모든 종류의 보석을 적어도 1개 이상 포함하는 가장 짧은 구간
+    s = set()
     for g in gems:
-        gem.add(g)
-    gemd = dict()
+        s.add(g)
+    # 목표 개수
+    t = len(s)
+    min_l = float('inf')
+    l = 0
+    d = dict()
     for r in range(len(gems)):
-        gemd[gems[r]] = gemd.get(gems[r] , 0) + 1
-        while len(gemd) >= len(gem):
-            if r - l < minL:
-                minL = r - l
-                minStart = l
-            gemd[gems[l]] -= 1
-            if gemd[gems[l]] == 0:
-                gemd.pop(gems[l])
+        gem = gems[r]
+        d[gem] = d.get(gem,0) + 1
+        while len(d.keys()) >= t:
+            remove = gems[l]
+            d[remove] = d.get(remove) - 1
+            if d[remove] == 0:
+                del d[remove]
             l += 1
-    answer = [minStart + 1, minStart + minL + 1]
-    return answer
+            cur_l = r + 1 - l
+            if cur_l < min_l:
+                ans = [l,r+1]
+                min_l = cur_l
+    return ans
