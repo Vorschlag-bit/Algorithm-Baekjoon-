7c9c4370167c8134aa2c817ffc726f3afd30303f
Lee Woo Jin <bustout@naver.com>
2025-10-10 20:09:33 +0900

    [level 3] Title: 징검다리 건너기, Time: 242.93 ms, Memory: 17.5 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,20 +1,26 @@
 def solution(stones, k):
     r = max(stones)
     l = 1
-    ans = 0
     while r >= l:
         # 최대로 건널 수 있는 사람의 수
         mid = (r+l) // 2
         flag = True
-        # 연속 건너기가 k이상이면 실패
-        ps = 0
-        for s in stones:
-            if s < mid:
-                ps += 1
-                if ps >= k:
-                    flag = False
+        idx = -1
+        while idx < len(stones) - 1:
+            move = False
+            for d in range(k,0,-1):
+                # 다 넘어갔다면
+                if idx + d >= len(stones):
+                    idx = len(stones)
+                    move = True
                     break
-            else: ps = 0
+                if stones[idx+d] >= mid:
+                    idx += d
+                    move = True
+                    break
+            if not move:
+                flag = False
+                break
         
         # 다 건널 수 있다면 l을 늘리기
         if flag:
@@ -22,5 +28,4 @@ def solution(stones, k):
         # 다 건널 수 없다면 r을 줄이기
         else:
             r = mid - 1
-    print(l,r,ans)
     return r
