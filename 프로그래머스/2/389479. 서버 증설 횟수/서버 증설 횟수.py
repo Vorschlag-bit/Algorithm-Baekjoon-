c589715a79104059160c02cb33fdecf41c533e99
Lee Woo Jin <bustout@naver.com>
2025-10-28 15:35:33 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.17 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
@@ -1,25 +1,26 @@
 from collections import deque
-import math
 def solution(players, m, k):
-    stack = deque()
     ans = 0
-    # 매 시간마다 현재 서버 개수 계산(감당 가능한지)
-    # 서버 종료 시간을 stack 속에 저장(오름차순), 매 시간마다 stack top과 시간 비교 후,
-    # 현재 시각이 더 크면 감소
-    servers = 0
-    for idx,p in enumerate(players):
-        # 서버 종료
-        if stack:
-            while stack and stack[0][0] <= idx:
-                endTime,cnt = stack.popleft()
-                servers -= cnt
-        # 현재 감당 가능한 사람의 수
-        able = servers * m + (m-1)
-        if able < p:
-            need = math.ceil((p - able) / m)
-            ans += need
-            servers += need
-            # 종료 시간 누적(시간,개수)
-            stack.append((idx+k,need))
-    print(ans)
+    # 하루동안 최소 몇 번 증설해야하는지
+    # players[i] = i~i시 사이의 이용자 수
+    # m명 = 1대 서버 감당 가능
+    # 한 번 증설한 서버는 k 시간 동안 운영
+    # stack으로 stack[0]에 가장 빨리 증설한 서버 종료 시각 저장
+    # 매번 시각과 종료 시각을 비교하기
+    stack = deque()
+    for i,p in enumerate(players):
+        # 플레이하는 사람 없으면 pass
+        if p == 0: continue
+        st = i
+        et = i+1
+        while stack and stack[0] < et:
+            stack.popleft()
+        server = len(stack)
+        # 현재 감당 가능 인원보다 더 많은 이가 플레이할 경우
+        if server * m < p:
+            total = p // m
+            total -= server
+            for _ in range(total):
+                stack.append(st+k)
+            ans += total
     return ans
