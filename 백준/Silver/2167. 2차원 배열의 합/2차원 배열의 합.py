93f02825d0127bc2515541b0b7da8269486ff93e
Lee Woo Jin <bustout@naver.com>
2025-11-24 15:09:06 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 112 ms, Memory: 35480 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -1,24 +1,22 @@
 from sys import stdin as input
 
 n,m = map(int,input.readline().split())
-arr = []
-for _ in range(n):
-    arr.append([0] + list(map(int,input.readline().split())))
+
+# [원칙 1] 배열 크기는 반드시 (N+1) * (M+1)로
+prefix = [[0] * (m+1) for _ in range(n+1)]
+
+# 원본 데이터와 누적합을 동시에 처리
+for i in range(1,n+1):
+    arr = list(map(int,input.readline().split()))
+    for j in range(1,m+1):
+        # [원칙 2] (1,1)부터 채우기 & 2차원 누적합 공식 사용
+        prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1] + arr[j-1]
+
 k = int(input.readline())
 
-cmd = []
 for _ in range(k):
-    i,j,x,y = map(int,input.readline().split())
-    cmd.append((i,j,x,y))
-
-# 누적합 계산
-for i in range(n):
-    for j in range(1,m+1):
-        arr[i][j] += arr[i][j-1]
-
-# i,j부터 x,y 위치에 저장되어 있는 수의 합 구하기
-for i,j,x,y in cmd:
-    s = 0
-    for r in range(i,x+1):
-        s += arr[r-1][y] - arr[r-1][j-1]
-    print(s)
+    x1,y1,x2,y2 = map(int,input.readline().split())
+    # [원칙 3] 면적합 공식 사용
+    # x1,x2,y1,y2에 대한 좌표 보정할 필요 X
+    ans = prefix[x2][y2] - prefix[x1-1][y2] - prefix[x2][y1-1] + prefix[x1-1][y1-1]
+    print(ans)
