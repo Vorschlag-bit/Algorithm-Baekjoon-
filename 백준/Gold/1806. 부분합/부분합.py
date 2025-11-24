b464a9f839f30debe0f9a472c8bf0b26dc68aef5
Lee Woo Jin <bustout@naver.com>
2025-11-24 13:23:43 +0900

    [Gold IV] Title: 부분합, Time: 96 ms, Memory: 42168 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -2,14 +2,14 @@ from sys import stdin as input
 
 n,s = map(int,input.readline().split())
 arr = list(map(int,input.readline().split()))
-
 l = 0
+cur = 0
 ans = float('inf')
-cur_sum = 0
-for r in range(n):
-    cur_sum += arr[r]
-    while cur_sum >= s:
-        ans = min(ans, r-l+1)
-        cur_sum -= arr[l]
+
+for i in range(n):
+    cur += arr[i]
+    while cur >= s:
+        cur -= arr[l]
+        ans = min(ans, i-l+1)
         l += 1
-print(ans if ans != float('inf') else 0)
+print(ans) if ans != float('inf') else print(0)
