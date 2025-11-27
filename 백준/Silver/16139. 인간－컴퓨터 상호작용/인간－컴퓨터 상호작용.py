c2b30c0f36a4a13bc8239e71949bfe8a10f50c8e
Lee Woo Jin <bustout@naver.com>
2025-11-27 18:47:57 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 304 ms, Memory: 207028 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -5,33 +5,32 @@ from sys import stdin as input
 
 # 0-based, l,r 인덱스 포함
 s = input.readline().strip()
+n = len(s)
 q = int(input.readline())
 cmd = []
+
 for _ in range(q):
     a,l,r = input.readline().split()
     cmd.append((a,int(l),int(r)))
 
 # q = 20만, 문자열 길이 = 20만
-# a-z = key, [0] * len(s) = value
-char = dict()
-for i in range(26):
-    c = ord('a') + i
-    char[chr(c)] = [0] * (len(s) + 1)
+# a-z
+char = [[0] * (n + 1) for _ in range(26)]
 
 # idx와 char를 통해, 해당 char아 char 등장 배열의 idx에 등장했음을 + 1
 for i,c in enumerate(s):
-    arr = char[c]
-    arr[i+1] += 1
+    idx = ord(c) - 97
+    char[idx][i+1] += 1
 
 # 모든 알파벳 배열의 누적합
 for i in range(26):
-    num = ord('a') + i
-    c = chr(num)
-    arr = char[c]
-    for j in range(len(s)):
-        arr[j+1] += arr[j]
+    for j in range(1, n+1):
+        char[i][j] += char[i][j-1]
     
 # cmd 배열을 순회하면서 계산
+result = []
 for a,l,r in cmd:
-    arr = char[a]
-    print(arr[r+1] - arr[l])
+    idx = ord(a) - 97
+    result.append(str(char[idx][r+1] - char[idx][l]))
+
+print('\n'.join(result))
